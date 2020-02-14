9ae0061a27648d8458b95ac2afb20a878908c38e
Nicolae <nicolae.marton@gmail.com>
2020-02-14 21:18:22 +0000

    tilemap package + basic controller script

---
+++ Assets/ControllerScripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+
+    private void FixedUpdate()
+    {
+        movePosition();
+    }
+
+    void processInput()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput * speed;
+        
+    }
+
+    void movePosition()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+}
