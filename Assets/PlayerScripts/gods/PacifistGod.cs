124e324475e0a6a019e61e589079a6aa5c1dc8df
frettini <nicolae.marton@gmail.com>
2020-02-15 19:36:43 +0000

    add god class and modify player in weapon test unity scene

---
+++ Assets/PlayerScripts/gods/PacifistGod.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace PlayerScripts.gods
+{
+    class PacifistGod : MonoBehaviour, God
+    {
+        Boon pacifistBoon;
+        Oath pacifistOath;
+
+        void Start()
+        {
+            //fireyBoon = ;
+            //fireyOath = ;
+        }
+
+        public new string name()
+        {
+            return "PacifistGod";
+        }
+
+        public Boon boon()
+        {
+            return pacifistBoon;
+        }
+
+        public Oath oath()
+        {
+            return pacifistOath;
+        }
+    }
+}
