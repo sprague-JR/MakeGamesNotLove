3899a1374cfde2d6a4d944bf2cbd57e858d13d93
sprague-JR <41627271+sprague-JR@users.noreply.github.com>
2020-02-15 14:54:31 +0000

    identified start and end of dungeon

--- Assets/DungeonMaker.cs
+++ Assets/DungeonMaker.cs
@@ -7,13 +7,41 @@ public class DungeonMaker : MonoBehaviour
     public int critical_path_length;
     List<Vector2Int> empty;
     public int min_rooms, max_rooms;
-    List<DungeonNode> rooms;
+	List<DungeonNode> rooms;
     public int x, y;
+
+	DungeonNode start;
+	DungeonNode end;
+
+
+	public Mapgenerator mg;
 
     void Start()
     {
+		mg = GetComponent<Mapgenerator>();
         makeRooms();
         makePath();
+		GetDiameterVertices();
+
+		foreach(DungeonNode dn in rooms)
+		{
+			
+
+
+			Vector2 tl = new Vector2(
+				(dn.room.gridLoc.x * 10) + dn.room.offset.x,
+				(dn.room.gridLoc.y * 10) + dn.room.offset.y + dn.room.dimensions.y
+				);
+
+
+			Vector2 br = new Vector2(
+				(dn.room.gridLoc.x * 10) + dn.room.offset.x + dn.room.dimensions.x,
+				(dn.room.gridLoc.y * 10) + dn.room.offset.y
+				);
+
+			mg.createmap(tl, br);
+
+		}
     }
 
     void makeRooms()
@@ -72,7 +100,7 @@ public class DungeonMaker : MonoBehaviour
                 else if (grid[current.room.gridLoc.x - 1, current.room.gridLoc.y] == null)
                 {
                     grid[current.room.gridLoc.x - 1, current.room.gridLoc.y] =
-                        new DungeonNode(new Room(current.room.gridLoc.x - 1, current.room.gridLoc.y, 10, 2, 2, true));
+                        new DungeonNode(new Room(current.room.gridLoc.x - 1, current.room.gridLoc.y));
                     dist.Add(grid[current.room.gridLoc.x - 1, current.room.gridLoc.y], dist[current] + 1);
                     toVisit.Add(grid[current.room.gridLoc.x - 1, current.room.gridLoc.y]);
                     prev.Add(grid[current.room.gridLoc.x - 1, current.room.gridLoc.y], current);
@@ -93,7 +121,7 @@ public class DungeonMaker : MonoBehaviour
                 else if (grid[current.room.gridLoc.x + 1, current.room.gridLoc.y] == null)
                 {
                     grid[current.room.gridLoc.x + 1, current.room.gridLoc.y] =
-                        new DungeonNode(new Room(current.room.gridLoc.x + 1, current.room.gridLoc.y, 10, 2, 2, true));
+                        new DungeonNode(new Room(current.room.gridLoc.x + 1, current.room.gridLoc.y));
                     dist.Add(grid[current.room.gridLoc.x + 1, current.room.gridLoc.y], dist[current] + 1);
                     toVisit.Add(grid[current.room.gridLoc.x + 1, current.room.gridLoc.y]);
                     prev.Add(grid[current.room.gridLoc.x + 1, current.room.gridLoc.y], current);
@@ -114,7 +142,7 @@ public class DungeonMaker : MonoBehaviour
                 else if (grid[current.room.gridLoc.x, current.room.gridLoc.y - 1] == null)
                 {
                     grid[current.room.gridLoc.x, current.room.gridLoc.y - 1] =
-                        new DungeonNode(new Room(current.room.gridLoc.x, current.room.gridLoc.y - 1, 10, 2, 2, true));
+                        new DungeonNode(new Room(current.room.gridLoc.x, current.room.gridLoc.y - 1));
                     dist.Add(grid[current.room.gridLoc.x, current.room.gridLoc.y - 1], dist[current] + 1);
                     toVisit.Add(grid[current.room.gridLoc.x, current.room.gridLoc.y - 1]);
                     prev.Add(grid[current.room.gridLoc.x, current.room.gridLoc.y - 1], current);
@@ -136,7 +164,7 @@ public class DungeonMaker : MonoBehaviour
                 else if (grid[current.room.gridLoc.x, current.room.gridLoc.y + 1] == null)
                 {
                     grid[current.room.gridLoc.x, current.room.gridLoc.y + 1] =
-                        new DungeonNode(new Room(current.room.gridLoc.x, current.room.gridLoc.y + 1, 10, 2, 2, true));
+                        new DungeonNode(new Room(current.room.gridLoc.x, current.room.gridLoc.y + 1));
                     dist.Add(grid[current.room.gridLoc.x, current.room.gridLoc.y + 1], dist[current] + 1);
                     toVisit.Add(grid[current.room.gridLoc.x, current.room.gridLoc.y + 1]);
                     prev.Add(grid[current.room.gridLoc.x, current.room.gridLoc.y + 1], current);
@@ -153,6 +181,7 @@ public class DungeonMaker : MonoBehaviour
             while (prev[current] != null)
             {
                 keep.Add(current);
+				current.AddConnection(prev[current]);
                 current = prev[current];
             }
         }
@@ -165,7 +194,7 @@ public class DungeonMaker : MonoBehaviour
                 {
                     grid[ix, iy].Destroy();
                     grid[ix, iy] = null;
-                }else if(!rooms.Contains(grid[ix,iy]))
+                }else if(!rooms.Contains(grid[ix,iy]) && grid[ix,iy] != null)
 				{
 					rooms.Add(grid[ix, iy]);
 				}
@@ -195,7 +224,7 @@ public class DungeonMaker : MonoBehaviour
 
         foreach (DungeonNode dn in visited)
         {
-            if (dist.ContainsKey(dn) && dist[dn] < minDist)
+            if (dist.ContainsKey(dn) && dist[dn] <= minDist)
             {
                 min = dn;
                 minDist = dist[min];
@@ -210,8 +239,87 @@ public class DungeonMaker : MonoBehaviour
                 }
             }
         }
-
-        Debug.Log(minDist);
         return min;
     }
+
+
+
+	void GetDiameterVertices()
+	{
+		List<DungeonNode> leaves = new List<DungeonNode>();
+
+
+
+		foreach(DungeonNode n in rooms)
+		{
+
+			if(n.connected.Count == 1)
+			{
+				leaves.Add(n);
+			}
+		}
+
+
+
+		int max_steps = 0;
+
+		foreach(DungeonNode start in leaves)
+		{
+			foreach(DungeonNode end in leaves)
+			{
+
+				int steps = path_distance(start, end);
+				if (steps > max_steps)
+				{
+					this.start = start;
+					this.end = end;
+					max_steps = steps;
+
+				}
+			}
+		}
+
+		Debug.Log(start.room.gridLoc);
+		Debug.Log(end.room.gridLoc);
+	}
+
+
+
+	private int path_distance(DungeonNode start, DungeonNode end)
+	{
+
+		Dictionary<DungeonNode, int> dist = new Dictionary<DungeonNode, int>();
+		List<DungeonNode> toVisit = new List<DungeonNode>();
+
+		dist.Add(start, 0);
+		toVisit.Add(start);
+
+
+
+		while(toVisit.Count > 0 && !dist.ContainsKey(end))
+		{
+			DungeonNode current = toVisit[0];
+			toVisit.RemoveAt(0);
+			//since all rooms have equal weight we do not need to sort by distance
+
+			foreach(DungeonNode connection in current.connected)
+			{
+				if(!dist.ContainsKey(connection))
+				{
+					dist.Add(connection, dist[current] + 1);
+					toVisit.Add(connection);
+					
+				}
+			}
+		}
+
+		if (!dist.ContainsKey(end))
+			return -1;
+		else
+			return dist[end];
+	}
 }
+
+
+
+
