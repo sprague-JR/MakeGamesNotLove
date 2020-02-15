d9681b1ee84f78966602874810c002557277844d
frettini <nicolae.marton@gmail.com>
2020-02-15 17:43:20 +0000

    Death Menu with replay and quit buttons

--- Assets/PlayerScripts/Player.cs
+++ Assets/PlayerScripts/Player.cs
@@ -6,19 +6,27 @@ namespace PlayerScripts
 {
     public class Player : MonoBehaviour
     {
-        public uint health;
+        public int health;
         public PlayerController controller;
+        public bool isDead=false;
         
         private Boon boon;
+
+        public void itsaDeadBoi()
+        {
+            //run animation
+            isDead = true; 
+        }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (other.collider.CompareTag($"Enemy"))
             {
                 Enemy enemy = other.gameObject.GetComponent<Enemy>();
-                health -= enemy.dmg();
+                health -= (int)enemy.dmg();
                 if (health == 0)
                 {
+                    itsaDeadBoi();
                     Debug.Log("Ya dead ya cunt");
                 }
             }
---
+++ Assets/ScnManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using PlayerScripts;
+
+public class ScnManager : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject deathUI;
+
+    private Player player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        deathUI = GameObject.Find("DeathUI");
+
+        deathUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.isDead)
+        {
+            playerDied();
+        }
+    }
+
+    void playerDied()
+    {
+        deathUI.SetActive(true);
+        isPaused = true;
+    }
+
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("aaaah coooome ooooon");
+        Application.Quit();
+    }
+}
