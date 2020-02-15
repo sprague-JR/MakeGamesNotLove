d1bb6dde271fa572f327550c0bbbb26b983c3749
Klara01-r <61062893+Klara01-r@users.noreply.github.com>
2020-02-15 16:03:08 +0000

    coridor

--- Assets/Mapgenerator.cs
+++ Assets/Mapgenerator.cs
@@ -16,7 +16,9 @@ public class Mapgenerator : MonoBehaviour
     {
         walkable = GameObject.Find("Walkable").GetComponent<Tilemap>();
         nonwalkable = GameObject.Find("Nonwalkable").GetComponent<Tilemap>();
-        createmap(new Vector2(-10, 4), new Vector2(8, -4));
+        createmap(new Vector2(-7, 5), new Vector2(-2, 3));
+        createmap(new Vector2(4, -1), new Vector2(7, -5));
+        coridor(new Vector2(-3, 3), new Vector2(6, -1));
     }
 
 
@@ -57,5 +59,81 @@ public class Mapgenerator : MonoBehaviour
         walkable.SetTile(new Vector3Int(x, y, 0), walkableTile);
     }
 
+    public void coridor(Vector2 pos1, Vector2 pos2)
+    {
+        int pos1X = Mathf.RoundToInt(pos1.x);
+        int pos1Y = Mathf.RoundToInt(pos1.y);
+        int pos2X = Mathf.RoundToInt(pos2.x);
+        int pos2Y = Mathf.RoundToInt(pos2.y);
+        bool horizontal = false;
+        int left = 1;
+        if (nonwalkable.HasTile(new Vector3Int(pos1X + 1, pos1Y, 0)) == false)
+        {
+            horizontal = true;
+        }
+        if (horizontal)
+        {
+            int temp = pos1X;
+            pos1X = pos1Y;
+            pos1Y = temp;
+            temp = pos1X;
+            pos2X = pos2Y;
+            pos2Y = temp;
 
+        }
+        if (nonwalkable.HasTile(new Vector3Int(pos1X + 2, pos1Y, 0)) == false)
+        {
+            pos1X--;
+        }
+        if (nonwalkable.HasTile(new Vector3Int(pos2X + 2, pos2Y, 0)) == false)
+        {
+            pos2X--;
+        }
+        if (pos1Y < pos2Y)
+        {
+            int temp = pos1Y;
+            pos1Y = pos2Y;
+            pos2Y = temp;
+        }
+        if (pos1X > pos2X)
+        {
+            left = -1;
+
+        }
+        int diff = Mathf.RoundToInt(Mathf.Abs(pos1Y - pos2Y)/2);
+        for(int i = 0; i< 6; i++)
+            {
+            pos1Y--;
+            nonwalkable.SetTile(new Vector3Int(pos1X+2, pos1Y, 0), nonwalkableTile);
+            walkable.SetTile(new Vector3Int(pos1X, pos1Y, 0), walkableTile);
+            nonwalkable.SetTile(new Vector3Int(pos1X-1, pos1Y, 0), nonwalkableTile);
+            walkable.SetTile(new Vector3Int(pos1X+1, pos1Y, 0), walkableTile);
+            }
+
+        diff = Mathf.Abs(pos1X - pos2X);
+        nonwalkable.SetTile(new Vector3Int(pos1X + (2*left)+(left - 1)/-2, pos1Y+1, 0), null);
+        nonwalkable.SetTile(new Vector3Int(pos1X + (2 * left)+(left -1)/(-2), pos1Y, 0), null);
+        pos1X = pos1X + 1-(left - 1) / (-2);
+        for (int i = 0; i <diff; i++)
+            {
+            pos1X= pos1X + 1*left;
+            nonwalkable.SetTile(new Vector3Int(pos1X, pos1Y+2 , 0), nonwalkableTile);
+            walkable.SetTile(new Vector3Int(pos1X, pos1Y , 0), walkableTile);
+            nonwalkable.SetTile(new Vector3Int(pos1X-(3*left), pos1Y-1 , 0), nonwalkableTile);
+            walkable.SetTile(new Vector3Int(pos1X, pos1Y+1 , 0), walkableTile);
+            }
+        diff = pos1Y - pos2Y;
+        nonwalkable.SetTile(new Vector3Int(pos1X + left, pos1Y + 1, 0), nonwalkableTile);
+        nonwalkable.SetTile(new Vector3Int(pos1X + left, pos1Y+2, 0), nonwalkableTile);
+        pos1X = pos1X - 1 + (left - 1) / (-2);
+        for (int i = 0; i < diff; i++)
+        {
+            pos1Y--;
+            nonwalkable.SetTile(new Vector3Int(pos1X + 2, pos1Y+(1+left)/2, 0), nonwalkableTile);
+            walkable.SetTile(new Vector3Int(pos1X, pos1Y, 0), walkableTile);
+            nonwalkable.SetTile(new Vector3Int(pos1X - 1, pos1Y+(1 -left) / 2, 0), nonwalkableTile);
+            walkable.SetTile(new Vector3Int(pos1X + 1, pos1Y, 0), walkableTile);
+        }
+
+    }
 }
